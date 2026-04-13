ccd734efbd70578ecd9e635e2cb49fdbfdb5d53d
Phaneendra <nphaneendra31@gmail.com>
2026-04-13 22:30:38 +0530

    done.

--- backend/main.py
+++ backend/main.py
@@ -135,7 +135,7 @@ def get_meal(data: UserData):
         """
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",   # Stable model
+            model="gemini-2.5-flash-lite",   # Stable model
             contents=prompt
         )
 
